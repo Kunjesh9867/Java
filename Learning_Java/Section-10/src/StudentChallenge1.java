84cc0690f844c4977260bae6993056bfbad38bd7
Kunjesh Ramani <kunjeshramani9867@gmail.com>
2023-04-02 16:31:37 -0400

    Files are added

---
+++ Learning_Java/Section-10/src/StudentChallenge1.java
@@ -0,0 +1,9 @@
+public class StudentChallenge1 {
+    static void primeNumber(int x){
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        primeNumber(4);
+
+    }
+}
